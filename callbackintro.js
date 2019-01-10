55740274649d8d9e8b1b34f5699a9814a3184a9d
Alvin Rao <alvinrao7@gmail.com>
2019-01-10 21:43:51 +0000

    Refactor to include forEach

--- callbackintro.js
+++ callbackintro.js
@@ -1,11 +1,10 @@
 // The second argument/parameter is expected to be a function
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(arr[i], i);   // execute callback
+  arr.forEach(function(name, i) {
+    if (name === "Waldo") {
+      found(name, i);
     }
   }
-}
 
 function actionWhenFound(name, i) {
   console.log('Found ' + name + ' at index', i);
@@ -13,29 +12,4 @@ function actionWhenFound(name, i) {
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
 
-// found.forEach(actionWhenFound)
 
-
-
-
-
-
-
-// var names = ["Alice", "Bob", "Waldo", "Winston"]
-
-// var printName = function(name, i) {
-//   console.log(name, ' at ', i);
-// }
-
-// names.forEach(printName)
-
-// arr.forEach(found())
-
-
-// var names = ['Khurram', 'Don', 'Joel', 'David', 'etc']
-
-// var printName = function(name, i) {
-//   console.log(name, ' at ', i);
-// }
-
-// names.forEach(printName)
